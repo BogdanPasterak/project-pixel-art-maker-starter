7111515818f459d85777a118841781f347f082dc
BogdanPasterak <bogdanpasterak@gmail.com>
2017-11-19 21:30:58 +0000

    first build fun.makeGrid

--- designs.js
+++ designs.js
@@ -2,9 +2,24 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+document.getElementById('sizePicker').setAttribute('onsubmit','return makeGrid()');
 
 function makeGrid() {
+  let height = document.getElementById('input_height').value;
+  let width = document.getElementById('input_width').value;
+  const target = document.getElementById('pixel_canvas');
+  let tr, td
 
-// Your code goes here!
+  target.innerHTML = ('');
+  for( let row = 0; row < height; row += 1){
+    tr = document.createElement('tr');
+    for( let col = 0; col < width; col += 1){
+      td = document.createElement('td');
+      tr.appendChild(td);
+    }
+    target.appendChild(tr);
+  }
 
+  console.log( height + '  ' + width );
+  return false;
 }
