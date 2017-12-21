281a1100d0739ab17efe5d7f836ce2cfccbe8e2a
BogdanPasterak <bogdanpasterak@gmail.com>
2017-12-21 21:41:28 +0000

    feat: dynamic building of the title

--- demo.js
+++ demo.js
@@ -1,9 +1,28 @@
 const startPage = () => {
   const title = ['L','a','b',':',' ','P','i','x','e','l',' ','A','r','t',' ','M','a','k','e','r'];
   const where = $('#title');
+  let oneDiv;
+
 
   title.forEach(function(char, index) {
-    where.append(char);
+    oneDiv = $('<div class="char_div">' + char + '</div>');
+    where.append(oneDiv);
+    // oneDiv.css('left', sumWidth);
+    // console.log(sumWidth + "  " + (oneDiv.width() | 0) + "  " + (oneDiv.width()));
+    // sumWidth += oneDiv.width() | 0;
+  });
+
+  setTimeout(setDystans,500);
+
+};
+
+const setDystans = () => {
+  let sumWidth = 50;
+
+  $('.char_div').each(function() {
+    $(this).css('left', sumWidth);
+    console.log(sumWidth + "  " + ($(this).width() | 0) + "  " + ($(this).width()));
+    sumWidth += $(this).width() | 0;
   });
 
 };
