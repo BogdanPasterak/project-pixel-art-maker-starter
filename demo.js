961a5ebb1b2ce2f66b9acb718d7517bdb3c45c15
BogdanPasterak <bogdanpasterak@gmail.com>
2017-12-28 18:26:22 +0000

    refactor:

--- demo.js
+++ demo.js
@@ -3,16 +3,17 @@ const startPage = () => {
   const title = (where.width() < 700) ? "Pixel Art".split("") : "Lab: Pixel Art Maker".split("");
   let oneDiv;
   let sumWidth = 0;
-  let overlap = 1;
+  let overlap = 0.8;
 
   title.forEach(function(char) {
-    oneDiv = $('<div class="char_div">' + char + '</div>');
+    oneDiv = $('<div class="char_div left_side">' + char + '</div>');
     where.append(oneDiv);
     sumWidth += oneDiv.width();
   });
+ // style="transform: rotateY(180deg);"
 
-  if ( sumWidth < where.width() - 20) {
-    sumWidth = ((where.width() - sumWidth) >> 1) + 7;
+  if ( sumWidth * overlap < where.width() - 20) {
+    sumWidth = ((where.width() - (sumWidth * overlap)) >> 1) + 7;
   } else {
     overlap = $('.char_div').last().width();
     overlap = (where.width() - 20 - overlap) / (sumWidth - overlap);
@@ -26,9 +27,14 @@ const startPage = () => {
 
   $('.char_div').each(function(index, element) {
     setTimeout( function() {
-      $(element).css('animation-name', 'turn_font');
-    }, ((title.length - index) * 100) );
-    console.log(index);
+      $(element).css('animation-name', 'turn_font1');
+      $(element).removeClass('left_side');
+    }, (index * 100 + 500) );
+    setTimeout( function() {
+      $(element).css('z-index', (title.length - index));
+      $(element).css('animation-name', 'turn_font2');
+    }, (index * 100 + 1500) );
+    //console.log(index);
   });
 
   // $('.char_div:nth(0)').delay('slow').css('animation-name', 'turn_font');
